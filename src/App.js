f2353e64d6f72db8146f3247d5edaf3c956b93cc
acwcheung <acwcheung1@gmail.com>
2020-02-15 21:10:40 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Square from './Components/Square';
+import CalcWinner from './Components/CalcWinner';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <Square />
+      <CalcWinner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CalcWinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function CalcWinner() {
+  return (
+    <div className="App">
+      Hello winner!
+    </div>
+  );
+}
+
+export default CalcWinner;
