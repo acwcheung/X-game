78779ccaf9687fce81243d53a883a9adbcbb3bda
acwcheung <acwcheung1@gmail.com>
2020-02-18 15:59:24 +0800

    do not display the move#0

--- src/Components/Steps.js
+++ src/Components/Steps.js
@@ -4,14 +4,16 @@ import React from 'react';
 function Steps(props) {
   const steps = props.history.map((step, i) => {
   	return(
-  		<button 
+      <button 
   		  key={i}
   		  onClick={() => props.onJump(i)}
   		>
   		  Go to move#{i}
   		</button>
   	)
-  })
+  });
+
+  
   return (
     <div className='move'>
       {steps}
