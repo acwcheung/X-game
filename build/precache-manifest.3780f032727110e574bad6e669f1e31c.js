3a20e13287246d2ad12f7e3d446febf9f660a2cf
acwcheung <acwcheung1@gmail.com>
2020-02-18 15:35:40 +0800

    deploy to github

---
+++ build/precache-manifest.3780f032727110e574bad6e669f1e31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ab357f3b554d62342f01681b09b9cb",
+    "url": "/X-game/index.html"
+  },
+  {
+    "revision": "16dfce63ec42cef0fa5a",
+    "url": "/X-game/static/css/main.73a7b83f.chunk.css"
+  },
+  {
+    "revision": "557b84b3bb4c1bcc8402",
+    "url": "/X-game/static/js/2.d44927ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/X-game/static/js/2.d44927ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16dfce63ec42cef0fa5a",
+    "url": "/X-game/static/js/main.51039855.chunk.js"
+  },
+  {
+    "revision": "d51976329921b3a4fbad",
+    "url": "/X-game/static/js/runtime-main.92b3007c.js"
+  }
+]);
